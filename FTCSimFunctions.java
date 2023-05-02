74509ad3b41c889ee476d5d7e6329cee56b78b0b
Paltrove <90796726+Paltrove@users.noreply.github.com>
2023-05-02 13:18:26 -0400

    Update FTCSimFunctions.java

--- FTCSimFunctions.java
+++ FTCSimFunctions.java
@@ -1,9 +1,20 @@
 //Allows Robot To Move With Little Code In Main
 public void drive(int powerL,int powerR, int time){
+  motorLeft.setDirection(DcMotor.Direction.FORWARD);
   motorLeft.setPower(powerL);
   motorRight.setPower(powerR);
   sleep(time);
   motorLeft.setPower(0);
   motorRight.setPower(0);
   sleep(300);
+}
+
+//Color Detection
+while (opModeIsActive()) {
+  if (color1.red() == 255) {
+    //Do Stuff
+    }
+  if (color1.blue() == 37) {
+    //Do Stuff
+  }
 }
