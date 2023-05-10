1637329d12d4ff0de057c33d45460566340e0d0d
Paltrove <90796726+Paltrove@users.noreply.github.com>
2023-05-10 13:24:21 -0400

    Update FTCSimFunctions.java

--- FTCSimFunctions.java
+++ FTCSimFunctions.java
@@ -1,13 +1,3 @@
-//Allows Robot To Move With Little Code In Main
-public void drive(int powerL,int powerR, int time){
-    motorLeft.setPower(powerL);
-    motorRight.setPower(powerR);
-    sleep(time);
-    motorLeft.setPower(0);
-    motorRight.setPower(0);
-    sleep(300);
-}
-
 //Color Detection
 if (color1.red() == 255) {
 }
@@ -53,5 +43,14 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             // Put loop blocks here
         }
+    }
+    
+    public void drive(int powerL,int powerR, int time){
+        motorLeft.setPower(powerL);
+        motorRight.setPower(powerR);
+        sleep(time);
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        sleep(300);
     }
 }
