34b4d1bfc0dcb314f31d1987586a22f75222a3dd
Paltrove <90796726+Paltrove@users.noreply.github.com>
2023-05-10 12:03:47 -0400

    Update FTCSimFunctions.java

--- FTCSimFunctions.java
+++ FTCSimFunctions.java
@@ -1,11 +1,11 @@
 //Allows Robot To Move With Little Code In Main
 public void drive(int powerL,int powerR, int time){
-  motorLeft.setPower(powerL);
-  motorRight.setPower(powerR);
-  sleep(time);
-  motorLeft.setPower(0);
-  motorRight.setPower(0);
-  sleep(300);
+    motorLeft.setPower(powerL);
+    motorRight.setPower(powerR);
+    sleep(time);
+    motorLeft.setPower(0);
+    motorRight.setPower(0);
+    sleep(300);
 }
 
 //Color Detection
@@ -21,3 +21,37 @@ if (distance1.getDistance(DistanceUnit.CM) < 225) {
 //Prints To Terminal
 telemetry.addData("Distance", (distance1.getDistance(DistanceUnit.CM)));
 telemetry.update();
+
+//Proper Spaceing
+
+/***********************************************************************
+*                                                                      *
+* OnbotJava Editor is still : beta! Please inform us of any bugs       *
+* on our discord channel! https://discord.gg/e7nVjMM                   *
+* Only BLOCKS code is submitted when in Arena                          *
+*                                                                      *
+***********************************************************************/
+
+
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    ColorSensor color1;
+    DistanceSensor distance1;
+    BNO055IMU imu;
+
+@Override
+    public void runOpMode() {
+        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+        color1 = hardwareMap.get(ColorSensor.class, "color1");
+        distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        // Put initialization blocks here
+        waitForStart();
+        // Put run blocks here
+        while (opModeIsActive()) {
+            // Put loop blocks here
+        }
+    }
+}
